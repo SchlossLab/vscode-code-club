ae21c40d27f7c62c0d73e113992648f8e04ac5dc
sklucas <salucas@umich.edu>
2021-10-29 17:48:37 -0400

    reorganize files

--- code/make_plot.R
+++ code/make_plot.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 
-table <- read_csv("demo-files/favorites_combined.csv") 
+table <- read_csv("results/favorites_combined.csv") 
     
 
 ggplot(table, aes(x=sunrise_or_sunset,fill=sunrise_or_sunset)) +
@@ -15,4 +15,4 @@ ggplot(table, aes(x=sunrise_or_sunset,fill=sunrise_or_sunset)) +
           plot.title = element_text(size=20)) +
     scale_fill_manual(values=c("#92b8db","#e49338"))
     
-ggsave("demo-files/plot.png")
+ggsave("results/plot.png")
